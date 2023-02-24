1c1c2ccff753705dfacec5848ae8a973e12d0150
rihernandez <rhc921004@gmail.com>
2023-02-24 10:29:08 -0400

    chore: drwaing map

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
+
 
 let map: google.maps.Map;
-
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const myLatLng = { lat: 18.5001200, lng: -69.9885700 };
+
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 10,
+      center: myLatLng,
+    }
+  );
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
   });
 }
 
